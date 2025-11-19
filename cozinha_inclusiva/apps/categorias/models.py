5f02e0d9632b6239a5267bd113024f16e88d37a1
Giselle Souza Novaes de Sá <giix.ns@gmail.com>
2025-11-19 18:07:57 -0300

    Implementação do Models de Categoria

--- cozinha_inclusiva/apps/categorias/models.py
+++ cozinha_inclusiva/apps/categorias/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+
+    nome = models.CharField(max_length=100, unique=True, verbose_name="Nome da Categoria")
+    visualizacoes = models.IntegerField(default=0)
+    receita_popular = models.BooleanField(default=False, verbose_name="É Receita Popular")
+    receitas_vinculadas = models.IntegerField(default=0)
+    data_criacao = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+    
+    def __str__(self):
+        return self.nome
