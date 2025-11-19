d740f190793fd38205d45b592a9f1f67174ec40c
Giselle Souza Novaes de Sá <giix.ns@gmail.com>
2025-11-19 16:23:09 -0300

    Criando o projeto DJANGO e aplicatvos

---
+++ cozinha_inclusiva/app/administrativo/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdmnistrativoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admnistrativo'
---
+++ cozinha_inclusiva/cozinha_inclusiva/__init__.py
@@ -0,0 +1,21 @@
+# Criando projeto e aplcativos
+'''
+django-admin startproject cozinha_inclusiva
+python manage.py startapp website
+python manage.py startapp admnistrativo
+python manage.py startapp receitas
+python manage.py startapp categorias
+python manage.py startapp comentarios
+'''
+
+# Atualizando Banco de Dados
+'''
+python manage.py makemigrations
+python manage.py migrate
+'''
+
+# Iniciando 
+'''
+cd cozinha_inclusiva
+python manage.py runserver
+'''
