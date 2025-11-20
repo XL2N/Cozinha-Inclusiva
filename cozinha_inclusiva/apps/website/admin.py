6c38e0eb637d181edb7b52faf0ffe19c8dcb076c
Giselle Souza Novaes de Sá <giix.ns@gmail.com>
2025-11-20 00:21:06 -0300

    Implementação do models do aplicativo website.

--- cozinha_inclusiva/apps/website/admin.py
+++ cozinha_inclusiva/apps/website/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ConfiguracaoWebsite
 
-# Register your models here.
+@admin.register(ConfiguracaoWebsite)
+class ConfiguracaoWebsiteAdmin(admin.ModelAdmin):
+
+    # Oculta a lista e o botão "Adicionar" na visualização da lista, 
+    def has_add_permission(self, request):
+        return not ConfiguracaoWebsite.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False # Não permite deletar a única linha
+    
+    list_display = ('titulo_site', 'email_contato')
