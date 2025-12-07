5225f9858ff5501c76eae17cb628b054f64bee0b
Giselle Souza Novaes de Sá <giix.ns@gmail.com>
2025-12-07 00:46:02 -0300

    criação de base.html e base.css

--- cozinha_inclusiva/apps/website/views.py
+++ cozinha_inclusiva/apps/website/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'website/inicio.html')
