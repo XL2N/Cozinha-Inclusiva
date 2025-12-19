dc173592b4a02bffe6784d3235d73bb48239baf8
Giselle Souza Novaes de Sá <giix.ns@gmail.com>
2025-12-18 22:55:59 -0300

    tela de categorias já buscando no banco

--- cozinha_inclusiva/apps/categorias/views.py
+++ cozinha_inclusiva/apps/categorias/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Categoria
+
 def gerenciar_categorias(request):
-    return render(request, 'categorias/gerenciar_categorias.html')
+    categorias = Categoria.objects.all()
+    return render(request, 'categorias/gerenciar_categorias.html', {'categorias': categorias})
